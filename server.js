0205996a35addbb7accb702d7c19fc9328ae3821
Maggie <zach.benton.27@gmail.com>
2023-07-06 19:37:54 -0400

    added success and error routes for sign up

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const bcrypt = require('bcrypt');
 let date = new Date().toISOString();
 
 const server = express();
+server.use(express.json());
+
 
 const cn = {
     host: 'localhost',
@@ -26,12 +28,33 @@ server.get('/heartbeat', (req, res) => {
 })
 
 server.post('/signup', async (req, res) => {
-    const {name, newUsername, email, password, birthday} = req.body;
+    const {newName, newUsername, newEmail, newPassword, newBirthday} = req.body;
     /* The line `let search = await db.query(SELECT * FROM (users) WHERE (newUsername === username));`
     is attempting to perform a database query to search for a user with a matching `newUsername` and
     `username`. */
-    let search = await db.query(`SELECT * FROM (users) WHERE (${newUsername} === username)`);
-    console.log(search);
+    let results = [];
+    let search = await db.query(`SELECT * FROM users WHERE username='${newUsername}'`);
+    let search2 = await db.query(`SELECT * FROM users WHERE email='${newEmail}'`);
+    if (search && search.length > 0 || search2 && search2.length > 0) {
+        results.push(search);
+        results.push(search2);
+        console.log(results.length);
+    }
+
+    if (results.length > 0) {
+        console.log('error: user already exists');
+        res.json('error: user already exists');
+    } else {
+        console.log(newPassword);
+        console.log(newEmail);
+        console.log(newUsername);
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(newPassword, salt);
+        await db.query(`INSERT INTO users (name, username, password, email, birthday) VALUES ('${newName}', '${newUsername}', '${newEmail}', '${hashedpassword}', '${newBirthday}')`);
+        console.log('running else');
+        res.json({message: 'joined successfully'})
+
+    }
 })
 
 const PORT = 8080;
