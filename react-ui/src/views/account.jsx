9246cdbe80dd6c78a67648419d12cc5996ca7f56
Maggiejackss <121919568+Maggiejackss@users.noreply.github.com>
2023-07-08 13:54:45 -0400

    010/accountpage (#8)
    * add express server
    
    * add sql file for db setup
    
    * add db instance in server file
    
    * commit for checkout
    
    * added success and error routes for sign up
    
    * add signin route
    
    * add basic JSX for account page and slice for favorites
    
    * add user verification on login and sets username in state
    
    * add store for username persistence and favorites data persistence
    
    * add signup component updates and features
    
    * add signup form
    
    * add server rendering of react

--- react-ui/src/views/account.jsx
+++ react-ui/src/views/account.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import { useState, useSelector } from "react";
+import { favesData } from "../features/favoritesSlice";
+import { Dispatch } from "react";
+import { addUsernameToStore } from "../features/usernameSlice";
 
 const Account = () => {
+    const dispatch = Dispatch();
+    let [userFavorites, setUserFavorite] = useState('');
+    let [userName, setUserName] = useState('');
+    let username = res.username;
+    const sendUsername =  dispatch(addUsernameToStore(userName));
+    let faves = useSelector(favesData);
+    setUserFavorite(faves);
+
     return (
-        <h1>Discover</h1>
+        <h1>account</h1>
     )
-};
+}
 
 export default Account;
--- react-ui/src/views/signup.jsx
+++ react-ui/src/views/signup.jsx
@@ -1,8 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
 
 const SignUp = () => {
+    const [name, setName] = useState();
+    const [username, setUser] = useState();
+    const [password, setPass] = useState();
+    const [email, setEmail] = useState();
+    const [birthday, setBirthday] = useState();
+    
+    const clearUseState = () => {
+        setName('');
+        setBirthday('');
+        setEmail('');
+        setUser('');
+        setPass('');
+    }
+
+    const handleName = e => {
+        setAttemptedName(e.target.value)
+    }
+
+    const handleUsername = e => {
+        setAttemptedUser(e.target.value)
+    }
+
+    const handlePassword = e => {
+        setAttemptedPass(e.target.value)
+    }
+
+    const handleEmail = e => {
+        setAttemptedEmail(e.target.value)
+    }
+
+    const handleBday = e => {
+        setAttemptedBday(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        fetch('/signup', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // TODO fix the JSON formatting for the body after full link up
+            body: JSON.stringify(name, username, email, password, birthday)
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            dispatch(addUsernameToStore(result.username));
+            location.href(result.redirectTo)
+        })
+        clearUseState();
+    }
     return (
-        <h1>Sign Up</h1>
+        <>
+            <h1>Sign Up</h1>
+            <form>
+                <input type='text' id='name' value={name} onChange={handleName}/>
+                <input type='text' id='username' value={username} onChange={handleUsername}/>
+                <input type='text' id='email' value={email} onChange={handleEmail} />
+                <input type='text' id='password' value={password} onChange={handlePassword}/>
+                <input type='text' id='birthday' value={birthday} onChange={handleBday}/>
+                <button type='submit' id='submitBtn' onClick={handleSubmit}/> 
+            </form>
+        </>
     )
 };
 
