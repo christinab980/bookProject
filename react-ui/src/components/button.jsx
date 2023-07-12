e434231d5c0ffc4d29bc23adfb35aa851bc58aed
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-07-11 17:15:41 -0700

    conditionally statement logged-in functionality fixed

--- react-ui/src/components/button.jsx
+++ react-ui/src/components/button.jsx
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { setFavorite } from '../features/favoritesSlice';
 import { setHaveRead } from '../features/haveReadSlice';
+import { username } from "../features/usernameSlice"
 
 
 const Button = ({bookRank, url, book}) => {
 
     let dispatch = useDispatch()
+    const loggedInData = useSelector(username)
+
+    useEffect(() => {
+        if (loggedInData.length > 0) {
+            setLoggedIn(true)
+        }
+    }, [])
 
     const [ openDropDown, setOpenDropDown ] = useState(false);
     const [ favorite, setFavoriteCheckBox ] = useState(false);
-    const [ haveRead, setHaveReadCheckBox ] = useState(false);
+    const [ haveReadCheckBox, setHaveReadCheckBox ] = useState(false);
     const [ loggedIn, setLoggedIn] = useState(false);
     const [ haveRead, setHaveRead ] = useState(false);
 
@@ -25,35 +33,13 @@ const Button = ({bookRank, url, book}) => {
     }
 
     function handleBooks()  {
-        setHaveReadCheckBox(!haveRead)
+        setHaveReadCheckBox(!haveReadCheckBox)
         dispatch(setHaveRead(book))
     }
 
     return (
         <>
         {loggedIn ? (
-                <div className='dropdown'>
-                <button onClick={handleOptions} className='dropdown-button'> Choose One </button>
-                {openDropDown && <div className='dropdown-menu'>
-                    <div key={bookRank}>
-                        <a
-                            className="dropdown-menu-items" 
-                            href="/sign-in"
-                            target="_blank">
-                                Sign In
-                        </a>
-                    </div>
-                    <div key={bookRank}>
-                        <a
-                            className="dropdown-menu-items" 
-                            href="/sign-up" 
-                            target="_blank">
-                                Sign Up
-                        </a>
-                    </div>
-                </div>}
-            </div>
-        ): (
             <div className='dropdown'>
                 <button onClick={handleOptions} className='dropdown-button'> Choose One </button>
                 {openDropDown && <div className='dropdown-menu'>
@@ -76,14 +62,36 @@ const Button = ({bookRank, url, book}) => {
                     <label>
                         <input 
                             type="checkbox" 
-                            value={haveRead} 
+                            value={haveReadCheckBox} 
                             onChange={handleBooks} 
                             />
                             Have Read
                     </label>
                 </div>}
             </div> 
-            )
+            ) : (
+                <div className='dropdown'>
+                <button onClick={handleOptions} className='dropdown-button'> Choose One </button>
+                {openDropDown && <div className='dropdown-menu'>
+                    <div key={bookRank}>
+                        <a
+                            className="dropdown-menu-items" 
+                            href="/sign-in"
+                            target="_blank">
+                                Sign In
+                        </a>
+                    </div>
+                    <div key={bookRank}>
+                        <a
+                            className="dropdown-menu-items" 
+                            href="/sign-up" 
+                            target="_blank">
+                                Sign Up
+                        </a>
+                    </div>
+                </div>}
+            </div>
+        )
     }
         </>
     )
