1243a150b4b8d61477bd329bba913a5fa3570d78
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-07-12 14:33:09 -0700

    resolving merge conflicts

--- react-ui/src/components/button.jsx
+++ react-ui/src/components/button.jsx
@@ -20,8 +20,6 @@ const Button = ({bookRank, url, book}) => {
     const [ favorite, setFavoriteCheckBox ] = useState(false);
     const [ haveReadCheckBox, setHaveReadCheckBox ] = useState(false);
     const [ loggedIn, setLoggedIn] = useState(false);
-    // const [ haveRead, setHaveRead ] = useState(false);
-
 
     function handleOptions() {
         setOpenDropDown(!openDropDown)
