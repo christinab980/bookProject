b94f2acfefbd98fafbd418189caeb7a1488c417f
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-07-18 15:02:23 -0700

    updating modal classes and options

--- react-ui/src/components/genreButtons.jsx
+++ react-ui/src/components/genreButtons.jsx
@@ -8,7 +8,7 @@ import { username } from '../features/usernameSlice';
 const Genre = () => {
     let [genres, setGenres] = useState('');
     const [btnClicked, setBtnClicked] = useState(false);
-    const [isActive, setIsActive] = useState("")
+    const [isActive, setIsActive] = useState([]);
 
     let books = 'books';
     let _username = useSelector(username);
@@ -35,95 +35,99 @@ const Genre = () => {
     }
 
     function handleGenre (clickedGenre) {
-        setBtnClicked(true)
-        setIsActive(clickedGenre)
 
-        let category = clickedGenre
-        setGenres(`${genres} ${category}`);
+            let indexFound = isActive.indexOf(clickedGenre)
 
+            if(indexFound === -1 && isActive.length < 3) {
+                setIsActive([...isActive, clickedGenre])
+            } else {
+                let filterActive = isActive.filter( genre => genre !== clickedGenre )
+                setIsActive(filterActive)
+            }
+        
+
+            let category = clickedGenre
+            setGenres(`${genres} ${category}`);
+        
     }
     
+    console.log(isActive)
 
     return (
         <>
             <div className="genre-buttons">
                 <button 
                     onClick={() => handleGenre("family")} 
-                    className={isActive === "family" ? "active" : " "}
+                    className={isActive.includes("family") ? "active" : " "}
                     data-attribute="family" 
                     href='genres'>
                         Family
                 </button>
                 <button 
                     onClick={() => handleGenre("fiction")} 
-                    className={isActive === "fiction" ? "active" : " "}
+                    className={isActive.includes("fiction") ? "active" : " "}
                     data-attribute="fiction">
                         Fiction
                 </button>
                 <button 
                     onClick={() => handleGenre("non-fiction")} 
-                    className={isActive === "non-fiction" ? "active" : " "}
+                    className={isActive.includes("non-fiction") ? "active" : " "}
                     data-attribute="non-fiction">
                         Non-Fiction 
                 </button>
                 <button 
                     onClick={() => handleGenre("manga")} 
-                    className={isActive === "manga" ? "active" : " "}
+                    className={isActive.includes("manga") ? "active" : " "}
                     data-attribute="manga">
                         Manga
                 </button>
                 <button 
                     onClick={() => handleGenre("science")} 
-                    className={isActive === "science" ? "active" : " "}
+                    className={isActive.includes("science") ? "active" : " "}
                     data-attribute="science">
                         Science
                 </button>
                 <button 
                     onClick={() => handleGenre("sports")} 
-                    className={isActive === "sports" ? "active" : " "}
+                    className={isActive.includes("sports") ? "active" : " "}
                     data-attribute="sports">
                         Sports
                 </button>
                 <button 
                     onClick={() => handleGenre("young-adults")} 
-                    className={isActive === "young-adults" ? "active" : " "}
+                    className={isActive.includes("young-adults") ? "active" : " "}
                     data-attribute="young-adults">
                         Young Adults
                 </button>
                 <button 
                     onClick={() => handleGenre("travel")} 
-                    className={isActive === "travel" ? "active" : " "}
+                    className={isActive.includes("travel") ? "active" : " "}
                     data-attribute="travel">
                         Travel
                 </button>
                 <button  
                     onClick={() => handleGenre("education")} 
-                    className={isActive === "education" ? "active" : " "}
+                    className={isActive.includes("education") ? "active" : " "}
                     data-attribute="education">
                         Education
                 </button>
                 <button 
                     onClick={() => handleGenre("paperback-advice")} 
-                    className={isActive === "paperback-advice" ? "active" : " "}
+                    className={isActive.includes("paperback-advice") ? "active" : " "}
                     data-attribute="paperback-advice">
                         Advice
                 </button>
                 <button 
                     onClick={() => handleGenre("food-and-fitness")} 
-                    className={isActive === "food-and-fitness" ? "active" : " "}
+                    className={isActive.includes("food-and-fitness") ? "active" : " "}
                     data-attribute="food-and-fitness">
                         Food and Fitness
                 </button>
                 <button 
                     onClick={() => handleGenre("humor")} 
-                    className={isActive === "humor" ? "active" : " "}
+                    className={isActive.includes("humor") ? "active" : " "}
                     data-attribute="humor">
                         Humor
-                </button>
-                <button
-                    onClick={handleSubmit}
-                    data-attribute='submit'>
-                        submit
                 </button>
             </div>
         </>
--- react-ui/src/sectioning/header.jsx
+++ react-ui/src/sectioning/header.jsx
@@ -5,12 +5,13 @@ import logo from "../navs/logo.json"
 import "/index.css"
 
 const Header = () => {
+
     const navs = data.map(nav => <NavLink key={nav.href} to={nav.href}>{nav.name}</NavLink>);
     const home = logo.map(nav => <NavLink key={nav.href} to={nav.href}><img className="logo" src={nav.src} /> </NavLink>)
 
     return (
         <header>
-            <div className="y-navs y-wrap">{home} {navs} </div>
+            <div className="y-navs y-wrap">{home}{navs}</div>
         </header>
     );
 };
