2844ee30497549e14b31d00561530ec833ad8203
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-07-18 13:40:29 -0700

    updating modal classnames

--- react-ui/src/components/genreButtons.jsx
+++ react-ui/src/components/genreButtons.jsx
@@ -7,6 +7,9 @@ import { username } from '../features/usernameSlice';
 
 const Genre = () => {
     let [genres, setGenres] = useState('');
+    const [btnClicked, setBtnClicked] = useState(false);
+    const [isActive, setIsActive] = useState("")
+
     let books = 'books';
     let _username = useSelector(username);
 
@@ -31,8 +34,11 @@ const Genre = () => {
         })
     }
 
-    function handleGenre (e) {
-        let category = e.target.getAttribute("data-attribute")
+    function handleGenre (clickedGenre) {
+        setBtnClicked(true)
+        setIsActive(clickedGenre)
+
+        let category = clickedGenre
         setGenres(`${genres} ${category}`);
 
     }
@@ -42,64 +48,75 @@ const Genre = () => {
         <>
             <div className="genre-buttons">
                 <button 
-                    onClick={handleGenre} 
+                    onClick={() => handleGenre("family")} 
+                    className={isActive === "family" ? "active" : " "}
                     data-attribute="family" 
                     href='genres'>
                         Family
                 </button>
                 <button 
-                    onClick={handleGenre}
+                    onClick={() => handleGenre("fiction")} 
+                    className={isActive === "fiction" ? "active" : " "}
                     data-attribute="fiction">
                         Fiction
                 </button>
                 <button 
-                    onClick={handleGenre} 
+                    onClick={() => handleGenre("non-fiction")} 
+                    className={isActive === "non-fiction" ? "active" : " "}
                     data-attribute="non-fiction">
                         Non-Fiction 
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("manga")} 
+                    className={isActive === "manga" ? "active" : " "}
                     data-attribute="manga">
                         Manga
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("science")} 
+                    className={isActive === "science" ? "active" : " "}
                     data-attribute="science">
                         Science
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("sports")} 
+                    className={isActive === "sports" ? "active" : " "}
                     data-attribute="sports">
                         Sports
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("young-adults")} 
+                    className={isActive === "young-adults" ? "active" : " "}
                     data-attribute="young-adults">
                         Young Adults
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("travel")} 
+                    className={isActive === "travel" ? "active" : " "}
                     data-attribute="travel">
                         Travel
                 </button>
                 <button  
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("education")} 
+                    className={isActive === "education" ? "active" : " "}
                     data-attribute="education">
                         Education
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("paperback-advice")} 
+                    className={isActive === "paperback-advice" ? "active" : " "}
                     data-attribute="paperback-advice">
                         Advice
                 </button>
                 <button 
-                    onClick= {handleGenre} 
-                    
+                    onClick={() => handleGenre("food-and-fitness")} 
+                    className={isActive === "food-and-fitness" ? "active" : " "}
                     data-attribute="food-and-fitness">
                         Food and Fitness
                 </button>
                 <button 
-                    onClick= {handleGenre} 
+                    onClick={() => handleGenre("humor")} 
+                    className={isActive === "humor" ? "active" : " "}
                     data-attribute="humor">
                         Humor
                 </button>
