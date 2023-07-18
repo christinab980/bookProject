b24799bcbefabe256b7488218e59160bd1acabde
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-07-18 16:09:42 -0700

    changing header to display logged in status - starting to

--- react-ui/src/sectioning/header.jsx
+++ react-ui/src/sectioning/header.jsx
@@ -5,8 +5,11 @@ import logo from "../navs/logo.json"
 import "/index.css"
 
 const Header = () => {
+    const isLoggedIn = false 
 
-    const navs = data.map(nav => <NavLink key={nav.href} to={nav.href}>{nav.name}</NavLink>);
+    const navs = data
+        .filter(nav => nav.isPrivate === isLoggedIn)
+        .map( nav => <NavLink key={nav.href} to={nav.href}>{nav.name}</NavLink>);
     const home = logo.map(nav => <NavLink key={nav.href} to={nav.href}><img className="logo" src={nav.src} /> </NavLink>)
 
     return (
