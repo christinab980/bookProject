3643bb67de5c425dbb18fc8a08bed155f7906ced
Christina Barron <71195516+christinab980@users.noreply.github.com>
2023-07-21 22:36:14 -0700

    Feature why (#26)
    * updating landing page
    
    * testing style sheet
    
    * updating landing genres
    
    * fixing cors issue
    
    * deployment issues - update
    
    * updating styles for mobile
    
    * updating modal classnames
    
    * updating modal classes and options
    
    * updated modal to update genres in store
    
    * change component modal and account page due to changes based off genreButton component
    
    * changing header to display logged in status - starting to
    
    * creating middleware and updating server to include cookies
    
    * debug
    
    * fixing recorded slice information and displaying the right recommended books
    
    * changed input type on passwords
    
    * added styles to account page
    
    * updating landing page
    
    * matts notes
    
    * trying to transfer from favorites to recommended genre
    
    * fixing nav bar
    
    * adding console.log
    
    * deleting unwanted files
    
    * debugging
    
    * dedugging and transfering from favorites to genres
    
    * updating styles
    
    * last changes
    
    * updating url

--- react-ui/src/components/landingButtons.jsx
+++ react-ui/src/components/landingButtons.jsx
@@ -8,62 +8,62 @@ const LandingPopularGenres = () => {
                 <a
                     className='genre-landing-component-a'
                     data-attribute="family" 
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Family
                 </a>
                 <a 
                     data-attribute="fiction"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Fiction
                 </a>
                 <a 
                     data-attribute="non-fiction"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Non-Fiction 
                 </a>
                 <a 
                     data-attribute="manga"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Manga
                 </a>
                 <a 
                     data-attribute="science"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Science
                 </a>
                 <a 
                     data-attribute="sports"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Sports
                 </a>
                 <a 
                     data-attribute="young-adults"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Young Adults
                 </a>
                 <a 
                     data-attribute="travel"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Travel
                 </a>
                 <a  
                     data-attribute="education"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Education
                 </a>
                 <a 
                     data-attribute="paperback-advice"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Advice
                 </a>
                 <a 
                     data-attribute="food-and-fitness"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Food and Fitness
                 </a>
                 <a 
                     data-attribute="humor"
-                    href='https://book-project-kdip.vercel.app/genres'>
+                    href='https://next-book-7hcurg108-christinab980.vercel.app/genres'>
                         Humor
                 </a>
             </div>
